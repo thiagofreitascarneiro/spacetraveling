f5d7d07c9f3e0e9e54e0539c2ca4f058bca1e3f9
Thiago <thiagofreitascarneiro@hotmail.com>
2022-02-11 22:55:34 -0300

    api of prismic dont load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <>
       <Header />
       <Component {...pageProps} />
-      <Home />
+      <Home postsPagination={undefined} />
       
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,30 @@
 /* eslint-disable */ 
 // eslint-disable-next-line
 
-import { GetStaticProps } from 'next';
+
 
 // se refere as propriedades que o componente pode receber
-import { AppProps } from 'next/app';
 
-import { getPrismicClient } from '../services/prismic';
 
+
+
+// SASS
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-import Head from 'next/head';
-import Prismic from '@prismicio/client';
 
+//Next
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { GetStaticProps } from 'next';
+
+//Prismic
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom'
+import { getPrismicClient } from '../services/prismic';
 
 interface Post {
+  slug: string;
   uid?: string;
   first_publication_date: string | null;
   data: {
@@ -34,19 +43,23 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
-  const todo = ''
 
+export default function Home({ posts }: HomeProps ) {
   return(
     <>  
       <main className={styles.Container}>
         <div className={styles.PostPage}>
-            <a>
-              <h1><strong>Como utilizar Hooks </strong></h1>
-              <time>5 de feveireiro de 2022</time>
-              <p>Pensando em sincronização 
-                em vez de ciclos de vida.</p>
-            </a>
+         { posts.map(post => (
+          <a key={post.slug}>
+            <h1><strong>{post.title} </strong></h1>
+            <time>{post.updatedAt}</time>
+            <p>{post.excerpt}</p>
+          </a>
+
+         )) 
+         
+         }
+            
         </div>
       </main>
     </>
@@ -60,15 +73,29 @@ export const getStaticProps: GetStaticProps = async () => {
   Prismic.predicates.at('document.type', 'post')
 ], {
     fetch: ['post.title', 'post.content'],
-    pageSize: 20,
+    pageSize: 5,
 })
 
   console.log(JSON.stringify(postsResponse, null, 2))
-  const post = ''
+  
+  const posts = postsResponse.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date,
+        data: {
+          title: post.data.title as string,
+          subtitle: post.data.content.heading.find(content => content.type === 'paragraph')?.text ?? '',
+          author: post.data.author as string,
+        },
+    }
+    
+  })
 
+  console.log('posts11234', posts)
+  
   return {
     props: {
-      post
+      posts
     }
   }
 };
