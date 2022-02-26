2bd769e6230da9531e41ba5ca4b7f159aae254e2
Thiago <thiagofreitascarneiro@hotmail.com>
2022-02-25 22:32:43 -0300

    create Routes in the slug page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,6 +2,7 @@
 
 //Next
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
 
 //Prismic
@@ -14,14 +15,17 @@ import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 //React
+
 import { useEffect, useState } from 'react';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { MdOutlinePersonOutline } from 'react-icons/md'
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
   data: {
     title: string;
+    subtitle: any;
     banner: {
       url: string;
     };
@@ -39,7 +43,8 @@ interface PostProps {
   post: Post;
 }
 
- export default function Post(){
+ export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
 
 return (
       <>
@@ -63,41 +68,51 @@ return (
 
 export const getStaticPaths : GetStaticPaths = async () => {
   const prismic = getPrismicClient();
-  const postsResponse = await prismic.query([
+  const posts = await prismic.query([
     Prismic.Predicates.at('document.type', 'post')
   ], {
-    fetch: ['posts.title', 'post.banner', 'posts.author', 'post.content'],
+    fetch: ['post'],
     pageSize:1,
   });
 
-  return {
-    paths: [
-      
+  const response = posts.results.map(post => {
+    return { params: {slug: post.uid} };
+  })
 
-    ],
-    fallback: 'blocking'
+  return {
+    paths: response,
+    fallback: true
   
   }
-  console.log('gestaticprops', JSON.stringify(postsResponse, null, 2))
-  // const todo = ''
+  
+  console.log('gestaticprops', JSON.stringify(posts, null, 2))
 };
 
-export const getStaticProps = async ({params}) => {
+
+export const getStaticProps = async ({ params }) => {
 
   const { slug } = params;
 
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('post', String(slug), {});
 
-  const post = { 
-    first_publication_date: response?.data.first_publication_date?? null,
+  const post: Post = { 
+    uid: response.uid,
+    first_publication_date: response?.data.first_publication_date,
     data: {
-      title: response?.data.title,
-      subtitle: response?.data.subtitle,
-      author: response?.data.author,
-      banner: response?.data.banner,
-      content: RichText.asHtml(response?.data.content)   
-    },
+      title: Array.isArray(response.data.title) ? 
+      RichText.asText(response.data.title) : response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: response.data.banner,
+      content: response.data.content.map((value: any) => {
+        return {
+          heading: value.heading,
+          body: value.body,
+        };
+      }),
+    }
+ 
   };
   return { 
     props: {
